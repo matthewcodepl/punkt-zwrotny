22d01b8d44fe2788809ef7473b6a6e906ac3e1fe
matthewcodepl <testy12221@gmail.com>
2019-09-25 21:20:42 +0200

    update navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,198 +1,119 @@
-import React, { Component } from 'react'
-import { Link } from "gatsby"
+    import { Link } from "gatsby"
 import styled from 'styled-components'
 import logo from '../images/logo.png'
+// import posed from 'react-pose'
+import React, { useState, useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
 import './layout.css';
-import posed from 'react-pose'
-
-
-const Nav = styled.div`
+const Menu = styled.nav`
     display:flex;
     justify-content:space-around;
     align-items:center;
-    padding: 10px;
-    background: rgb(41,41,41);
-    color:#fff;
+    background:rgb(41,41,41);
+    padding:15px 0;
     position:relative;
     @media(max-width:800px) {
         justify-content:space-between;
-        padding:5px;
+        padding:15px 10px;
     }
 `;
 
-
-
-
-const ListMenu = posed.ul({
-    open: {x:'0%' },
-    closed: {x:'-100%'}
-});
-
-
-
-const StyledListMenu = styled(ListMenu)`
-   list-style:none;
+const Navigation = styled.ul`
     display:flex;
-    transition:.4s ease-in-out;
+    list-style:none;
     @media(max-width:800px) {
         position:absolute;
         top:100%;
         left:0%;
+        background:black;
         flex-direction:column;
-        background:#171717;
         width:100%;
         text-align:center;
-
     }
-  ${({isDesktop}) => !isDesktop && `
-        // justify-content: space-between;
-  `}
 `;
 
-const MenuItem = styled.li`
+const NavigationLink = styled.li`
     margin-left:25px;
     @media(max-width:800px) {
-        margin:0px;
-        padding:15px 0px;
+        padding:15px 0;
     }
 `;
 
-
-const Linkstyled = styled(Link)`
-    color:#fff;
+const StyledLink =styled(Link)`
+    color: #fff;
     text-decoration:none;
-    transition:opacity .3s ease-in-out;
+    transition:.3s ease-in-out;
     &:hover {
         opacity:.6;
     }
 `;
 
-const Hamburger = styled.button`
-  padding: 10px;
-  display: inline-block;
-  cursor: pointer;
-  background-color: transparent;
-  border: 0;
-  margin: 0;
-  cursor:pointer;
-  @media(min-width:800px) {
-      display:none;
-  }
-`;
-
-
-const Hamburgerbox = styled.span`
-  width: 35px;
-  height: 24px;
-  display: inline-block;
-  position: relative;
-`;
-const HamburgerInner = styled.span`
-  width: 100%;
-  height: 3px;
-  background-color: #fff;
-  position: absolute;
-  left: 0;
-  top: 50%;
-  transform: translateY(-50%);
-  transition: background-color .1s .2s ease-in-out;
-  &::before,
-  &::after {
-
-  width: 100%;
-  height: 3px;
-  background-color: #fff;
-  position: absolute; 
-  content: '';
-  left: 0;
-  transition: transform .2s .2s ease-in-out;
-  }
-  &::before {
-      top:-10px;
-  }
-  &::after {
-      top:10px;
-  }
-`;
-
-
-
-
-
-export default class navigation extends Component {
-    state = {
-        isMenuOpen: false,
-        isDesktop: false
+const ToggleBurger = styled.button`
+    display:none;
+    @media(max-width:800px) {
+        display:inline-block;
+        background:transparent;
+        border:0px;
+        font-size:30px;
     }
-    componentDidMount() {
-        this.updateWindowDimensions();
-      }
-    
-    updateWindowDimensions = () => {
-        if (window.innerWidth >=800) {
-          this.setState({ isDesktop: true, isMenuOpen: true });
-        } else {
-          this.setState({ isDesktop: false, isMenuOpen: false });
-        }
-      }
-    
-      componentWillUnmount() {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-        // window.removeEventListener('scroll', this.updateScrollPosition);
-      }
-    
-    render() {
-   
-    
-        const {isMenuOpen, isDesktop} = this.state 
-        const handleClick = () => {
-            this.setState({
-                isMenuOpen: !this.state.isMenuOpen
-            })
-            // console.log(this.state.isMenuOpen)
-    
-        }
-    
+`;
 
-        return (
-            <>
-                
-        <Nav id="nav">
-                <img src={logo} height="70px" alt="Logo"></img>
-            <Hamburger
-            isOpen ={this.state.active}
-            onClick={handleClick}
-            >
-                <Hamburgerbox>
-                    <HamburgerInner>
-                    </HamburgerInner>
-                </Hamburgerbox>
-            </Hamburger>
-            <StyledListMenu 
-            id="#nav"
-            pose={isMenuOpen ? 'open' : 'closed'}
-            isDesktop={isDesktop}
-            >
-                    <MenuItem>
-                        <Linkstyled to="/">Strona główna </Linkstyled>
-                    </MenuItem>
-                    <MenuItem>
-                        <Linkstyled to="/o-nas">O nas </Linkstyled>
-                    </MenuItem>
 
-                    <MenuItem>
-                        <Linkstyled to="/wspolpraca">Współpraca </Linkstyled>
-                    </MenuItem>
+export default function Header() {
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-                    <MenuItem>
-                        <Linkstyled to="/kontakt">Kontakt </Linkstyled>
-                    </MenuItem>
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
 
-            </StyledListMenu>
-        </Nav>
-            </>
-        )
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = mediaQuery => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
     }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
+
+  return (
+    <Menu >
+      <img width="50px" src={logo}  alt="logo" />
+      <CSSTransition
+        in={!isSmallScreen || isNavVisible}
+        timeout={350}
+        classNames="NavAnimation"
+        unmountOnExit
+      >
+        <Navigation className="Nav">
+            <NavigationLink>
+                <StyledLink href="/">Strona główna</StyledLink>
+            </NavigationLink>
+            <NavigationLink>
+                <StyledLink href="/">O nas</StyledLink>
+            </NavigationLink>       
+            <NavigationLink>
+                <StyledLink href="/">Współpraca</StyledLink>
+            </NavigationLink>     
+            <NavigationLink>
+                <StyledLink href="/">Kontakt</StyledLink>
+            </NavigationLink>
+        </Navigation>
+      </CSSTransition>
+      <ToggleBurger onClick={toggleNav}>
+        🍔
+      </ToggleBurger>
+    </Menu>
+  );
 }
 
     
